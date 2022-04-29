5e54c55bb5ae13a7bf38c970706f48d0933ba878
aman <aman.nagle1999@gmail.com>
2022-04-30 02:11:24 +0530

    completed sumALL exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    if(x<0 || typeof(y)!="number")
+    {
+        return "ERROR"
+    }
+    let sum=0;
+    for(let i=x;i<=y;i++)
+    sum+=i;
+    for(let i=x;i>=y;i--)
+    sum+=i;
+    return sum
 };
 
 // Do not edit below this line
