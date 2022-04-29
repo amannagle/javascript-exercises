fe36abbaf59e59ed1edf5ad9d537b56b76dcf56e
aman <aman.nagle1999@gmail.com>
2022-04-29 21:27:20 +0530

    completed removefromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...nums) {
+    for (const num of nums)
+    {
+        let index=arr.indexOf(num)
+        if (index == -1)
+        continue;
+        arr.splice(index,1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
