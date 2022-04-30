e717dd141c935b1c92bd664b8f7b18846eaee0c5
aman <aman.nagle1999@gmail.com>
2022-04-30 17:07:23 +0530

    done 07 temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let result;
+  result=((temp-32)*5)/9;
+  result=parseFloat(result.toFixed(1));
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let result;
+  result=(temp*9)/5+32;
+  result=parseFloat(result.toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
