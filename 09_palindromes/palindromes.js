20f634c436902f1c6cef35bf748174773b140763
aman <aman.nagle1999@gmail.com>
2022-04-30 21:12:27 +0530

    done with palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str=str.toLowerCase();
+    str =str.replace(/[^\w\s\']|_/g, "")
+         .replace(/\s+/g, " ").replace(/\s/g,'');
+    let rev='';
+    for(let i=str.length-1;i>=0;i--)
+    {
+        rev+=str.charAt(i);
+    }
+    rev=rev.toLowerCase().replace(/\s/g,'');
+    return str===rev
 };
 
 // Do not edit below this line
