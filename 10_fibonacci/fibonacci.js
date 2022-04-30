ecf1e6b6eeca5070c769d10d302673220f0d21f4
aman <aman.nagle1999@gmail.com>
2022-04-30 22:42:35 +0530

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n=parseInt(n);
+    if( n<0)
+        return "OOPS"
+        if (n==1)
+        return 1;
+        if (n==0)
+        return 0;
+    for(let i=0;i<=n-2;i++)
+    {
+        
+        let arr=[0,1]
+        let sum=0;
+        for(let i=0;i<=n-2;i++)
+        {
+            sum=arr[i]+arr[i+1];
+            arr[i+2]=sum;
+        }
+        return arr[n];
+    }
 };
 
 // Do not edit below this line
