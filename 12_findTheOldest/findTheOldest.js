f33dcb667f5d2e177f024476f59dd3dee45b5a1d
aman <aman.nagle1999@gmail.com>
2022-05-02 13:08:28 +0530

    completed find oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objArr) {
+    return objArr.reduce((x,y)=>{
+        if(x.yearOfDeath == undefined)
+        x.yearOfDeath=new Date().getFullYear() 
+        if((x.yearOfDeath-x.yearOfBirth)>(y.yearOfDeath-y.yearOfBirth))
+        return x;
+        return y;
+    })
 };
 
 // Do not edit below this line
