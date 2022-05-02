daccb72fa6eadb2be1f61e2105673e6839d3d50f
aman <aman.nagle1999@gmail.com>
2022-05-02 17:11:15 +0530

    added caesar function logic

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,10 +1,20 @@
-const caesar = function(str) {
-    str=str.map(x=>caesar(x))
+const caesar = function(str,shift) {
+    return str.split("").map(s=>caesar(s,shift))
+    .join("")
 
 };
-function caesar()
+function mod(x,y)
 {
-    
+    return ((x % y) + y) % y;
+}
+function caesar(str,shift)
+{
+    let current_char;
+    let starter=0;
+    starter=str.charCodeAt();
+    current_char=starter;
+    starter=starter<97? 65: 97;
+    return String.fromCharCode((mod(current_char+shift-starter),starter)+starter);
 }
 // Do not edit below this line
 module.exports = caesar;
